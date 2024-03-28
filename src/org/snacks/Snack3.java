ede48936633fe73bf1c95c53b2d18d48554244e3
Salvatore Bono <salvatorebono2001@gmail.com>
2024-03-28 15:15:29 +0100

    start snack 4

--- src/org/snacks/Snack3.java
+++ src/org/snacks/Snack3.java
@@ -2,6 +2,14 @@ package org.snacks;
 
 public class Snack3 {
     public static void main(String[] args) {
-
+    /*Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari*/
+        int[] num = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] % 2 != 0){
+                sum += num[i];
+            }
+        }
+        System.out.println(sum);
     }
 }
---
+++ src/org/snacks/Snack4.java
@@ -0,0 +1,7 @@
+package org.snacks;
+
+public class Snack4 {
+    public static void main(String[] args) {
+
+    }
+}
