6570dc66617ca9060a6f2c97025e0d69ac07c821
Salvatore Bono <salvatorebono2001@gmail.com>
2024-03-28 15:45:08 +0100

    snack 3 correction

--- src/org/snacks/Snack3.java
+++ src/org/snacks/Snack3.java
@@ -6,7 +6,7 @@ public class Snack3 {
         int[] num = {1,2,3,4,5,6,7,8,9,10};
         int sum = 0;
         for (int i = 0; i < num.length; i++) {
-            if (num[i] % 2 != 0){
+            if (i % 2 != 0){
                 sum += num[i];
             }
         }
--- src/org/snacks/Snack4.java
+++ src/org/snacks/Snack4.java
@@ -1,7 +1,15 @@
 package org.snacks;
 
+import java.util.Scanner;
+
 public class Snack4 {
     public static void main(String[] args) {
-
+/*Data in input una stringa verificare se è palindroma*/
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Verifica se palindroma");
+        String str = keyboard.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+            char singleChar = str.charAt(i);
+        }
     }
 }
